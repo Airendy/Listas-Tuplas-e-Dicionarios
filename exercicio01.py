df8d740a07888fff503dd5ddd6c8ad64e802b258
Rafaela Becker <65984117+Airendy@users.noreply.github.com>
2026-03-30 19:41:01 -0300

    Create exercicio01.py

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+lista_compras = ["arroz", "feijão", "peixe"]
+lista_compras.append("tomate")
+print("Lista de compras:", lista_compras)
+lista_compras.remove("arroz")
+print("Lista de compras:", lista_compras)
