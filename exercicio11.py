9286a64b89ba442ccafb2d6afb666ed9cf69a87b
Rafaela Becker <65984117+Airendy@users.noreply.github.com>
2026-03-30 20:07:33 -0300

    Create exercicio11.py

---
+++ exercicio11.py
@@ -0,0 +1,10 @@
+#Criar e exibir uma tupla simples
+#Tarefa: Leia dois nomes do usuário e coloque-os em uma tupla. Depois exiba a tupla e o tipo dela.
+
+nome1 = input("Digite o primeiro nome: ")
+nome2 = input("Digite o segundo nome: ")
+
+nomes = (nome1, nome2)
+
+print(nomes)
+print(type(nomes))
