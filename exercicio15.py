bcd3d7361498f5a4262d4de9e0d151792b8a27c0
Rafaela Becker <65984117+Airendy@users.noreply.github.com>
2026-03-30 20:15:45 -0300

    Create exercicio15.py

---
+++ exercicio15.py
@@ -0,0 +1,19 @@
+#Dias da semana com tuplas
+
+dias = (
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+    "Domingo"
+)
+
+print(dias)
+
+print("Primeiro dia:", dias[0])
+
+print("Último dia:", dias[-1])
+
+print("Quantidade de dias:", len(dias))
